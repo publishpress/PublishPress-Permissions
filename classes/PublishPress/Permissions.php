072aea5c6a50e4e92e6d9714dbdfe51312e257da
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2019-12-17 15:42:29 -0500

    Support 'presspermit_refresh_administrator_check' action
    If this action has been done, buffered results will not be returned for the isAdministrator() check.

--- classes/PublishPress/Permissions.php
+++ classes/PublishPress/Permissions.php
@@ -588,7 +588,7 @@ class Permissions
     {
         // @todo: any other Gutenberg Administrator requests to filter?
         return $this->isAdministrator($user_id, 'unfiltered', $args) 
-        && (!defined('REST_REQUEST') || ! REST_REQUEST || empty($_REQUEST['parent_exclude'])); // page parent dropdown
+        && (!defined('REST_REQUEST') || ! REST_REQUEST || (empty($_REQUEST['parent_exclude']) || did_action('presspermit_refresh_administrator_check'))); // page parent dropdown
     }
 
     public function isAdministrator($user_id = false, $admin_type = 'content', $args = [])
@@ -599,6 +599,8 @@ class Permissions
         static $cached_user_id = [];
 
         if (false === $user_id) {
+            $args['force_refresh'] = !empty($args['force_refresh']) || did_action('presspermit_refresh_administrator_check');
+
             if (
                 isset($is_administrator[$admin_type])
                 && ($cached_user_id[$admin_type] == $current_user->ID) && empty($args['force_refresh'])
@@ -910,12 +912,16 @@ class Permissions
         return !$error;
     }
 
+    public function isPro() {
+        return defined('PRESSPERMIT_PRO_VERSION') && !class_exists('PublishPress\Permissions\Core');
+    }
+
     /**
      * @return EDD_SL_Plugin_Updater
      */
     public function load_updater()
     {
-		if (defined('PRESSPERMIT_PRO_VERSION')) {
+		if ($this->isPro()) {
         	require_once(PRESSPERMIT_ABSPATH . '/includes-pro/library/Factory.php');
         	$container = \PublishPress\Permissions\Factory::get_container();
 			return $container['edd_container']['update_manager'];
@@ -924,7 +930,7 @@ class Permissions
     
     public function keyStatus($refresh = false)
     {
-        if (defined('PRESSPERMIT_PRO_VERSION')) {
+        if ($this->isPro()) {
             require_once(PRESSPERMIT_ABSPATH . '/includes-pro/pro-key.php');
             return _presspermit_key_status($refresh);
         } else {
