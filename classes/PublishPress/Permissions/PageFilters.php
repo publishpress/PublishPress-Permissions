64710633b8ef34ed3311d1402e7c175cf6799586
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2019-12-17 14:21:45 -0500

    Mirror shared code changes from PressPermit Pro 2.8.3

--- classes/PublishPress/Permissions/PageFilters.php
+++ classes/PublishPress/Permissions/PageFilters.php
@@ -54,6 +54,7 @@ class PageFilters
             'authors' => '',
             'parent' => -1,
             'exclude_tree' => '',
+            'exclude_parent' => '',
             'number' => '',
             'offset' => 0,
             'post_type' => 'page',
@@ -507,6 +508,18 @@ class PageFilters
             $remap_args = compact('child_of', 'parent', 'exclude', 'depth', 'orderby');  // one or more of these args may have been modified after extraction 
 
             \PressShack\Ancestry::remapTree($pages, $ancestors, $remap_args);
+        }
+
+        if (!defined('PRESSPERMIT_GET_PAGES_IGNORE_EXCLUDE_ARGS')) { // installations that rely on pre-2.8.3 behavior can enable this constant
+            foreach ($pages as $key => $page) {
+                if ($exclude && in_array($page->ID, $exclude)) {
+                    unset($pages[$key]);
+                }
+
+                if ($exclude_parent && in_array($page->post_parent, $exclude_parent)) {
+                    unset($pages[$key]);
+                }
+            }
         }
 
         if (!empty($append_page) && !empty($pages)) {
