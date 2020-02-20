e32083256c41b4eee58898d4b34751c4fe7501ad
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2020-02-19 19:32:35 -0500

    Pro promo message at top of PP Permissions screens

--- includes/Core.php
+++ includes/Core.php
@@ -4,6 +4,18 @@ namespace PublishPress\Permissions;
 class Core {
     function __construct() {
         add_filter('presspermit_options', [$this, 'fltPressPermitOptions'], 15);
+		add_action('presspermit_plugin_page_admin_header', [$this, 'adminHeader']);
+		
+	}
+	function adminHeader() {
+        $descript = sprintf(
+            __('You\'re using PublishPress Permissions Free. To unlock more features, consider upgrading to <a href="%s" target="_blank">Permissions Pro</a>.', 'press-permit-core'),
+            'https://publishpress.com/presspermit/'
+        );
+        ?>
+
+        <div id="presspermit-pro-notice" class="activating"><?php echo $descript; ?></div>
+        <?php
     }
 
     function fltPressPermitOptions($options) {
