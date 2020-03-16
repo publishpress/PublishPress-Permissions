3a9f2fde4318eb2f002e83adb9985303677254f7
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2020-03-16 00:44:56 -0400

    Revert "Group / User Permissions: use radio options for Operation, Adjustment"
    This reverts commit 3a37c7367264d05d4c17758dc33451cfdace3625.

--- common/js/exception-edit.dev.js
+++ common/js/exception-edit.dev.js
@@ -102,7 +102,7 @@ jQuery(document).ready(function ($) {
 
         var items = $('#menu-settings-column').find('.tabs-panel-active .categorychecklist li input:checked');
 
-        if (!$('input[name="pp_select_x_operation"]').val()) {
+        if (!$('select[name="pp_select_x_operation"]').val()) {
             $('#pp_item_selection_msg').html(ppRestrict.noOp);
             $('#pp_item_selection_msg').addClass('pp-error-note');
             $('#pp_item_selection_msg').show();
@@ -149,15 +149,15 @@ jQuery(document).ready(function ($) {
         }
 
         var for_type = $('select[name="pp_select_x_for_type"]').val();
-        var op = $('input[name="pp_select_x_operation"]:checked').val();
+        var op = $('select[name="pp_select_x_operation"]').val();
         var via_type = $('select[name="pp_select_x_via_type"]').val();
-        var mod_type = $('input[name="pp_select_x_mod_type"]:checked').val();
+        var mod_type = $('select[name="pp_select_x_mod_type"]').val();
 
         var for_type_caption = $('select[name="pp_select_x_for_type"] option:selected').html()
-        var op_caption = $('input[name="pp_select_x_operation"]:checked').next('span').html()
-        var via_type_caption = $('select[name="pp_select_x_via_type"] :selected').html()
-        var mod_type_caption = $('input[name="pp_select_x_mod_type"]:checked').next('span').html();
-        var assign_for_captions = $('input[name="pp_select_x_mod_type"]:checked').next('span').html()
+        var op_caption = $('select[name="pp_select_x_operation"] option:selected').html()
+        var via_type_caption = $('select[name="pp_select_x_via_type"] option:selected').html()
+        var mod_type_caption = $('select[name="pp_select_x_mod_type"] option:selected').html();
+        var assign_for_captions = $('select[name="pp_select_x_mod_type"] option:selected').html()
 
         var conds = $('td.pp-select-x-status').find('input[name="pp_select_x_cond[]"]:checked');
 
@@ -277,14 +277,14 @@ jQuery(document).ready(function ($) {
     }
 
     var presspermitReloadViaType = function () {
-        if ($('input[name="pp_select_x_operation"]').val())
+        if ($('select[name="pp_select_x_operation"]').val())
             presspermitXajaxUI('get_via_type_options', presspermitDrawViaTypes);
         else
             $('.pp-select-x-via-type').hide();
     }
 
     var presspermitReloadModificationType = function () {
-        if ($('input[name="pp_select_x_operation"]').val()) {
+        if ($('select[name="pp_select_x_operation"]').val()) {
             //presspermitXajaxUI('get_mod_options',presspermitDrawModificationTypes);
             setTimeout(function () {
                 presspermitXajaxUI('get_mod_options', presspermitDrawModificationTypes)
@@ -304,8 +304,8 @@ jQuery(document).ready(function ($) {
     }
 
     var presspermitReloadStatus = function () {
-        var op = $('input[name="pp_select_x_operation"]').val();
-        var mod_type = $('input[name="pp_select_x_mod_type"]').val();
+        var op = $('select[name="pp_select_x_operation"]').val();
+        var mod_type = $('select[name="pp_select_x_mod_type"]').val();
         if (mod_type && op) {
             //presspermitXajaxUI('get_status_ui',presspermitDrawStatus);
             setTimeout(function () {
@@ -339,24 +339,11 @@ jQuery(document).ready(function ($) {
         $('#pp_add_exception').css('width', 'auto');
     });
 
-    $('td.pp-select-x-operation').click(function() {
-        var sel = $(this).find('input:checked').val();
-        if (sel) {
-            presspermitLastOp = sel;
-        }
-        presspermitReloadViaType();
-    });
+    $('select[name="pp_select_x_operation"]').bind('change', presspermitReloadViaType);
+    $('select[name="pp_select_x_operation"]').bind('change', presspermitReloadModificationType);
+    $('select[name="pp_select_x_operation"]').bind('change', presspermitReloadStatus);
 
-    $('td.pp-select-x-operation').bind('click', presspermitReloadModificationType);
-    $('td.pp-select-x-operation').bind('click', presspermitReloadStatus);
-
-    $('td.pp-select-x-mod-type').click(function() {
-        var sel = $(this).find('input:checked').val();
-        if (sel) {
-            presspermitLastModType = sel;
-        }
-        presspermitReloadStatus();
-    });
+    $('select[name="pp_select_x_mod_type"]').bind('change', presspermitReloadStatus);
 
     $('select[name="pp_select_x_via_type"]').bind('change', presspermitReloadStatus);
     $('select[name="pp_select_x_via_type"]').bind('change', presspermitReloadAssignFor);
@@ -377,19 +364,15 @@ jQuery(document).ready(function ($) {
         $('input.menu-item-checkbox').prop('checked', false);
     });
 
-    $('select[name="pp_select_x_via_type"]').click(function () {
-        presspermitLastViaType = $(this).val();
-    });
-
     var presspermitUpdateItemNoneCaption = function() {
         if ($('select[name="pp_select_x_for_type"]').val() == '_term_') {
-            if ($('input[name="pp_select_x_operation"]').val() == 'associate' && $('input[name="pp_select_x_mod_type"]').val() != 'additional') {
+            if ($('select[name="pp_select_x_operation"]').val() == 'associate' && $('select[name="pp_select_x_mod_type"]').val() != 'additional') {
                 $('#select-exception-' + $('select[name="pp_select_x_via_type"]').val()).find('input.menu-item-checkbox[value="0"]').siblings('label').first().html(ppRestrict.noParent);
             } else {
                 $('#select-exception-' + $('select[name="pp_select_x_via_type"]').val()).find('input.menu-item-checkbox[value="0"]').siblings('label').first().html(ppRestrict.none);
             }
         } else {
-            if ($('input[name="pp_select_x_operation"]').val() == 'associate' && $('input[name="pp_select_x_mod_type"]').val() != 'additional') {
+            if ($('select[name="pp_select_x_operation"]').val() == 'associate' && $('select[name="pp_select_x_mod_type"]').val() != 'additional') {
                 $('#select-exception-' + $('select[name="pp_select_x_for_type"]').val()).find('input.menu-item-checkbox[value="0"]').siblings('label').first().html(ppRestrict.noParent);
             } else {
                 $('#select-exception-' + $('select[name="pp_select_x_for_type"]').val()).find('input.menu-item-checkbox[value="0"]').siblings('label').first().html(ppRestrict.none);
@@ -397,34 +380,13 @@ jQuery(document).ready(function ($) {
         }
     }
 
-    $('input[name="pp_select_x_mod_type"]').bind('change', presspermitUpdateItemNoneCaption);
-
-    $(document).on('click', '#pp_select_x_item_assign', function(e){
-        presspermitLastItemAssign = $(this).prop('checked');
-    });
-
-    $(document).on('click', '#pp_select_x_child_assign', function(e){
-        presspermitLastChildAssign = $(this).prop('checked');
-    });
-
-    var presspermitLastOp = '';
-    var presspermitLastModType = '';
-    var presspermitLastViaType = '';
-    var presspermitLastItemAssign = '';
-    var presspermitLastChildAssign = '';
+    $('select[name="pp_select_x_mod_type"]').bind('change', presspermitUpdateItemNoneCaption);
 
     var presspermitDrawOperations = function (data, txtStatus) {
-        sel = $('td.pp-select-x-operation');
+        sel = $('select[name="pp_select_x_operation"]');
         sel.html(data);
         sel.triggerHandler('change');
         $('.pp-select-x-operation').show();
-
-        if (presspermitLastOp && $('input[name="pp_select_x_operation"][value="' + presspermitLastOp + '"]').length) {
-            $('input[name="pp_select_x_operation"][value="' + presspermitLastOp + '"]').click();
-        } else {
-            $('input[name="pp_select_x_operation"]').first().click();
-        }
-
         presspermitXajaxUI_done();
     }
 
@@ -433,26 +395,14 @@ jQuery(document).ready(function ($) {
         sel.html(data);
         sel.triggerHandler('change');
         $('.pp-select-x-via-type').show();
-
-        if (presspermitLastViaType && $('select[name="pp_select_x_via_type"] option[value="' + presspermitLastViaType + '"]').length) {
-            $('select[name="pp_select_x_via_type"]').val(presspermitLastViaType).change();
-        }
-
         presspermitXajaxUI_done();
     }
 
     var presspermitDrawModificationTypes = function (data, txtStatus) {
-        sel = $('td.pp-select-x-mod-type');
+        sel = $('select[name="pp_select_x_mod_type"]');
         sel.html(data);
         sel.triggerHandler('change');
         $('.pp-select-x-mod-type').show();
-
-        if (presspermitLastModType && $('input[name="pp_select_x_mod_type"][value="' + presspermitLastModType + '"]').length) {
-            $('input[name="pp_select_x_mod_type"][value="' + presspermitLastModType + '"]').click();
-        } else {
-            $('input[name="pp_select_x_mod_type"]').first().click();
-        }
-
         presspermitXajaxUI_done();
     }
 
@@ -464,14 +414,6 @@ jQuery(document).ready(function ($) {
             $('.pp-select-x-assign-for').show();
         else
             $('.pp-select-x-assign-for').hide();
-
-        if (typeof presspermitLastItemAssign === 'boolean' && $('#pp_select_x_item_assign:visible').length) {
-            $('#pp_select_x_item_assign:visible').prop('checked', presspermitLastItemAssign);
-        }
-
-        if ($('#pp_select_x_child_assign:visible').length) {
-            $('#pp_select_x_child_assign:visible').prop('checked', presspermitLastChildAssign);
-        }
 
         presspermitXajaxUI_done();
     }
@@ -504,9 +446,9 @@ jQuery(document).ready(function ($) {
         var data = {
             'pp_ajax_agent_exceptions': op,
             'pp_for_type': $('select[name="pp_select_x_for_type"]').val(),
-            'pp_operation': $('input[name="pp_select_x_operation"]').val(),
+            'pp_operation': $('select[name="pp_select_x_operation"]').val(),
             'pp_via_type': $('select[name="pp_select_x_via_type"]').val(),
-            'pp_mod_type': $('input[name="pp_select_x_mod_type"]').val(),
+            'pp_mod_type': $('select[name="pp_select_x_mod_type"]').val(),
             'pp_agent_id': ppRestrict.agentID,
             'pp_agent_type': ppRestrict.agentType,
             'pp_item_id': item_id
