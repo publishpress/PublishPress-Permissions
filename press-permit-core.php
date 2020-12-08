23b61ca7264e277835ed82cd97a3a084b57442af
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2020-12-08 16:41:18 -0500

    Release 3.3.10

--- press-permit-core.php
+++ press-permit-core.php
@@ -5,7 +5,7 @@
  * Description: Advanced yet accessible content permissions. Give users or groups type-specific roles. Enable or block access for specific posts or terms.
  * Author: PublishPress
  * Author URI:  https://publishpress.com/
- * Version:     3.3.9
+ * Version:     3.3.10-rc4
  * Text Domain: press-permit-core
  * Domain Path: /languages/
  * Min WP Version: 4.9.7
@@ -98,7 +98,7 @@ if ((!defined('PRESSPERMIT_FILE') && !$pro_active) || $presspermit_loaded_by_pro
 	        return;
 	    }
 	
-	    define('PRESSPERMIT_VERSION', '3.3.9');
+	    define('PRESSPERMIT_VERSION', '3.3.10-rc4');
 		
 	    if (!$presspermit_loaded_by_pro) {
 	        require_once(__DIR__ . '/includes/Core.php');
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: restrict, access, permissions, cms, user, private, category, pages, privac
 Requires at least: 4.9.7
 Tested up to: 5.5
 Requires PHP: 5.6.20
-Stable tag: 3.3.9
+Stable tag: 3.3.10
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
