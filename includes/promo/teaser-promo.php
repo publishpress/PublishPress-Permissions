619feaea0db0e0459c6425d49ee4e5c6914472f6
Valentín García <htmgarcia@gmail.com>
2021-03-01 15:56:48 -0600

    Update class names

--- includes/promo/teaser-promo.php
+++ includes/promo/teaser-promo.php
@@ -1,4 +1,4 @@
-<div class="wrap publishpress-caps-manage pressshack-admin-wrapper pp-conditions pp-capability-menus-wrapper-promo">
+<div class="wrap publishpress-caps-manage pressshack-admin-wrapper pp-conditions pp-permissions-menus-wrapper-promo">
     <header>
     <div class="pp-icon"><?php echo '<img src="' . PRESSPERMIT_URLPATH . '/common/img/publishpress-logo-icon.png" alt="" />';?></div>
     <h1>
@@ -9,12 +9,12 @@
     <table id="akmin">
         <tr>
             <td class="content">
-                <div id="pp-capability-menu-wrapper" class="postbox" style="box-shadow: none;">
-                    <div class="pp-capability-menus-promo">
-                        <div class="pp-capability-menus-promo-inner">
-                            <img src="<?php echo PRESSPERMIT_URLPATH . '/includes/promo/permissions-teaser-desktop.jpg';?>" class="pp-capability-desktop" />
-                            <img src="<?php echo PRESSPERMIT_URLPATH . '/includes/promo/permissions-teaser-mobile.jpg';?>" class="pp-capability-mobile" />
-                            <div class="pp-capability-menus-promo-content">
+                <div id="pp-permissions-menu-wrapper" class="postbox" style="box-shadow: none; background: none;">
+                    <div class="pp-permissions-menus-promo">
+                        <div class="pp-permissions-menus-promo-inner">
+                            <img src="<?php echo PRESSPERMIT_URLPATH . '/includes/promo/permissions-teaser-desktop.jpg';?>" class="pp-permissions-desktop" />
+                            <img src="<?php echo PRESSPERMIT_URLPATH . '/includes/promo/permissions-teaser-mobile.jpg';?>" class="pp-permissions-mobile" />
+                            <div class="pp-permissions-menus-promo-content">
                                 <p>
                                     <?php _e('Show a teaser message or login prompt for protected content. Available in PublishPress Permissions Pro.', 'press-permit-core'); ?>
                                 </p>
@@ -24,7 +24,7 @@
                                     </a>
                                 </p>
                             </div>
-                            <div class="pp-capability-menus-promo-gradient"></div>
+                            <div class="pp-permissions-menus-promo-gradient"></div>
                         </div>
                     </div>
                 </div>
