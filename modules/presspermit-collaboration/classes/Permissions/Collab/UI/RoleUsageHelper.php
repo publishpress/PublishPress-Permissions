1fc2a4cfc2b99a629254be72721e96f629a4bc34
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2021-12-20 09:17:28 -0500

    input sanitization

--- modules/presspermit-collaboration/classes/Permissions/Collab/UI/RoleUsageHelper.php
+++ modules/presspermit-collaboration/classes/Permissions/Collab/UI/RoleUsageHelper.php
@@ -10,7 +10,7 @@ class RoleUsageHelper
         if (empty($_REQUEST)) {
             $referer = '<input type="hidden" name="wp_http_referer" value="' . esc_attr(stripslashes($_SERVER['REQUEST_URI'])) . '" />';
         } elseif (!empty($_REQUEST['wp_http_referer'])) {
-            $redirect = remove_query_arg(['wp_http_referer', 'updated', 'delete_count'], stripslashes($_REQUEST['wp_http_referer']));
+            $redirect = remove_query_arg(['wp_http_referer', 'updated', 'delete_count'], sanitize_url($_REQUEST['wp_http_referer']));
             $referer = '<input type="hidden" name="wp_http_referer" value="' . esc_attr($redirect) . '" />';
         } else {
             $redirect = "$url?page=presspermit-role-usage";
