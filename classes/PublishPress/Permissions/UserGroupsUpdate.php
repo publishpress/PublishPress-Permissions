8e22782814abde046fad145061f79bcdf05a7663
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2022-02-16 21:38:46 -0500

    Remove unused variable assignment

--- classes/PublishPress/Permissions/UserGroupsUpdate.php
+++ classes/PublishPress/Permissions/UserGroupsUpdate.php
@@ -20,8 +20,6 @@ class UserGroupsUpdate
 			      }
 
             // by retrieving filtered groups here, user will only modify membership for groups they can administer
-            $is_administrator = $pp->isUserAdministrator();
-
             $posted_groups = (presspermit_is_POST($agent_type)) ? array_map('intval', presspermit_POST_var($agent_type)) : [];
 
             if ($omit_group_ids) {
