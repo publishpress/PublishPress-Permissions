27cc5b097cf81046e03cb8f89ca703f9cb7d029a
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2022-10-06 19:01:23 -0400

    Update beta version

--- press-permit-core.php
+++ press-permit-core.php
@@ -5,7 +5,7 @@
  * Description: Advanced yet accessible content permissions. Give users or groups type-specific roles. Enable or block access for specific posts or terms.
  * Author: PublishPress
  * Author URI:  https://publishpress.com/
- * Version:     3.8-beta6
+ * Version:     3.8-rc
  * Text Domain: press-permit-core
  * Domain Path: /languages/
  * Min WP Version: 4.9.7
@@ -135,7 +135,7 @@ if ((!defined('PRESSPERMIT_FILE') && !$pro_active) || $presspermit_loaded_by_pro
 	        return;
 	    }
 	
-		define('PRESSPERMIT_VERSION', '3.8-beta6');
+		define('PRESSPERMIT_VERSION', '3.8-rc');
 	    
 	    if (!defined('PRESSPERMIT_READ_PUBLIC_CAP')) {
 	        define('PRESSPERMIT_READ_PUBLIC_CAP', 'read');
