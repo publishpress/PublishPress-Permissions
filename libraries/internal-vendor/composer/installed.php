af6c38315b9ac8bef2fffc8aecb9f5502180eaa8
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2023-06-13 15:09:43 -0400

    Composer: updated libraries

--- libraries/internal-vendor/composer/installed.php
+++ libraries/internal-vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '653ea4e05ed168730f50e4c663d0d17dfa19f8f6',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '653ea4e05ed168730f50e4c663d0d17dfa19f8f6',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
