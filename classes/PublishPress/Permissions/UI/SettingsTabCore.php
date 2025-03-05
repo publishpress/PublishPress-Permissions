2c2f07e19ffe4c4d25695152380f0f20f9353ac7
Richa Ferry Setyawan <49858604+richaferry@users.noreply.github.com>
2025-03-06 03:27:39 +0700

    fix tooltip escape (#1484)

--- classes/PublishPress/Permissions/UI/SettingsTabCore.php
+++ classes/PublishPress/Permissions/UI/SettingsTabCore.php
@@ -187,8 +187,8 @@ class SettingsTabCore
 
                                         echo '</label>';
 
-                                        if(!empty($locked_types[$key]) && isset($key) && $key == 'attachment') {
-                                            echo esc_html($this->generateTooltip('This feature allows you to restrict access to Media Library files when they are accessed via WordPress. To restrict viewing of files when they are visited directly via the URL, use the "File Access" feature.', '', 'top', true));
+                                        if (!empty($locked_types[$key]) && isset($key) && $key == 'attachment') {
+                                            $this->generateTooltip(esc_html__('This feature allows you to restrict access to Media Library files when they are accessed via WordPress. To restrict viewing of files when they are visited directly via the URL, use the "File Access" feature.', 'press-permit-core'),'','top');
                                         }
 
                                         if (!empty($enabled[$key]) && isset($obj->capability_type) && !in_array($obj->capability_type, [$obj->name, 'post', 'page'])) {
@@ -354,13 +354,18 @@ class SettingsTabCore
 
     function generateTooltip($tooltip, $text = '', $position = 'top', $useIcon = true)
     {
-        $tooltipHtml = '<span data-toggle="tooltip" data-placement="' . $position . '" title="' . htmlspecialchars($tooltip) . '">';
-        $tooltipHtml .= htmlspecialchars($text);
+        $tooltipHtml = '<span data-toggle="tooltip" data-placement="' . esc_attr($position) . '" title="' . esc_attr($tooltip) . '">';
+        $tooltipHtml .= esc_html($text);
         if ($useIcon) {
             $tooltipHtml .= '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 50 50"><path d="M 25 2 C 12.264481 2 2 12.264481 2 25 C 2 37.735519 12.264481 48 25 48 C 37.735519 48 48 37.735519 48 25 C 48 12.264481 37.735519 2 25 2 z M 25 4 C 36.664481 4 46 13.335519 46 25 C 46 36.664481 36.664481 46 25 46 C 13.335519 46 4 36.664481 4 25 C 4 13.335519 13.335519 4 25 4 z M 25 11 A 3 3 0 0 0 25 17 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 23 23 L 23 36 L 21 36 L 21 38 L 29 38 L 29 36 L 27 36 L 27 21 L 21 21 z"></path></svg>';
         }
         $tooltipHtml .= '</span>';
 
-        return $tooltipHtml;
+        echo wp_kses($tooltipHtml, [
+            'span' => ['data-toggle' => true, 'data-placement' => true, 'title' => true],
+            'svg' => ['xmlns' => true, 'x' => true, 'y' => true, 'width' => true, 'height' => true, 'viewBox' => true],
+            'path' => ['d' => true]
+        ]);
+        return;
     }
 }
