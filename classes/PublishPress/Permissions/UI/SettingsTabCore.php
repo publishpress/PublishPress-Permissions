55d7593d1ea159594ad8409fd3161628c2dbdc04
Richa Ferry Setyawan <aakricha2@gmail.com>
2025-03-07 00:36:20 +0700

    fix tooltip escape char

--- classes/PublishPress/Permissions/UI/SettingsTabCore.php
+++ classes/PublishPress/Permissions/UI/SettingsTabCore.php
@@ -354,18 +354,17 @@ class SettingsTabCore
 
     function generateTooltip($tooltip, $text = '', $position = 'top', $useIcon = true)
     {
-        $tooltipHtml = '<span data-toggle="tooltip" data-placement="' . esc_attr($position) . '" title="' . esc_attr($tooltip) . '">';
-        $tooltipHtml .= esc_html($text);
-        if ($useIcon) {
-            $tooltipHtml .= '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 50 50"><path d="M 25 2 C 12.264481 2 2 12.264481 2 25 C 2 37.735519 12.264481 48 25 48 C 37.735519 48 48 37.735519 48 25 C 48 12.264481 37.735519 2 25 2 z M 25 4 C 36.664481 4 46 13.335519 46 25 C 46 36.664481 36.664481 46 25 46 C 13.335519 46 4 36.664481 4 25 C 4 13.335519 13.335519 4 25 4 z M 25 11 A 3 3 0 0 0 25 17 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 23 23 L 23 36 L 21 36 L 21 38 L 29 38 L 29 36 L 27 36 L 27 21 L 21 21 z"></path></svg>';
-        }
-        $tooltipHtml .= '</span>';
-
-        echo wp_kses($tooltipHtml, [
-            'span' => ['data-toggle' => true, 'data-placement' => true, 'title' => true],
-            'svg' => ['xmlns' => true, 'x' => true, 'y' => true, 'width' => true, 'height' => true, 'viewBox' => true],
-            'path' => ['d' => true]
-        ]);
-        return;
+        ?>
+        <span data-toggle="tooltip" data-placement="<?php esc_attr_e($position); ?>" title="<?php esc_attr_e($tooltip); ?>">
+        <?php 
+        esc_html_e($text);
+        if ($useIcon) : ?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 50 50" style="margin-left: 4px; vertical-align: text-bottom;">
+                <path d="M 25 2 C 12.264481 2 2 12.264481 2 25 C 2 37.735519 12.264481 48 25 48 C 37.735519 48 48 37.735519 48 25 C 48 12.264481 37.735519 2 25 2 z M 25 4 C 36.664481 4 46 13.335519 46 25 C 46 36.664481 36.664481 46 25 46 C 13.335519 46 4 36.664481 4 25 C 4 13.335519 13.335519 4 25 4 z M 25 11 A 3 3 0 0 0 25 17 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 23 23 L 23 36 L 21 36 L 21 38 L 29 38 L 29 36 L 27 36 L 27 21 L 21 21 z"></path>
+            </svg>
+        <?php
+        endif; ?>
+        </span>
+        <?php
     }
 }
