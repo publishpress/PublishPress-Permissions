a02cb2c5d571c01d56cafbf22f19c96235642d6b
Richa Ferry Setyawan <49858604+richaferry@users.noreply.github.com>
2025-03-27 07:00:37 +0700

    fix/#1251_unshow_note_item_in_core_tab (#1497)

--- common/js/settings.dev.js
+++ common/js/settings.dev.js
@@ -13,18 +13,26 @@ jQuery(document).ready(function ($) {
 
     // todo: pass img url variable, title
     if (ppCoreSettings.displayHints) {
-        $('.pp-options-table tr').each(function(i,e) {
-            if ($(this).find('td .pp-subtext, td .pp-hint').length) {
+        $('.pp-options-table tr').each(function (i,e) {
+            var $row = $(this); // Cache the current row for better performance
+
+            // Check for .pp-subtext elements
+            var subtextElements = $row.find('.pp-subtext, .pp-hint');
+            var hasSubtext = subtextElements.length > 0;
+
+            // Check if there is at least one .pp-subtext that does NOT have .pp-no-hide
+            var hasVisibleSubtext = subtextElements.filter(':not(.pp-no-hide)').length > 0;
+
+            // Append the image if the conditions are met
+            if (hasSubtext && hasVisibleSubtext) {
                 var img_html = '<img class="pp-show-hints" title="See more configuration tips..." src="' + ppCoreSettings.hintImg + '" />';
                 
-                if ($(e).find('div.pp-extra-heading').length) {
-                    $(e).find('div.pp-extra-heading').before(img_html);
+                if ($row.find('div.pp-extra-heading').length) {
+                    $row.find('div.pp-extra-heading').before(img_html);
+                } else if ($row.find('> th').length) {
+                    $row.find('> th').append(img_html);
                 } else {
-                    if ($(e).find('> th').length) {
-                        $(e).find('> th').append(img_html);
-                    } else {
-                        $(e).find('> td').first().find('span').first().append(img_html);
-                    }
+                    $row.find('> td').first().find('span').first().append(img_html);
                 }
             }
         });
